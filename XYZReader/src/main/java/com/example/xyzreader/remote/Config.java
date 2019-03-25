afcef2c53229f719c441fa49c1a749a8a3160398
Jose Escalante <jesca15341@gmail.com>
2019-03-24 21:38:51 -0400

    First Commit

---
+++ XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
@@ -0,0 +1,24 @@
+package com.example.xyzreader.remote;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Config {
+    private static String TAG = Config.class.toString();
+
+    public static URL baseUrl(Context context) {
+        URL url = null;
+        try {
+            url = new URL("https://go.udacity.com/xyz-reader-json" );
+        } catch (MalformedURLException ignored) {
+            Toast.makeText(context, "Please check your internet connection", Toast.LENGTH_SHORT).show();
+            Log.e(TAG, "Please check your internet connection.");
+        }
+
+        return url;
+    }
+}
